a1ae71126dd61e3e4afba2581403aa3bedbbe91b
RxNunez <Nunez.ryan@gmail.com>
2017-08-02 21:56:42 -0700

    include random

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,5 +1,5 @@
 package models;
-
+import java.util.Random;
 /**
  * Created by Guest on 8/2/17.
  */
@@ -35,6 +35,17 @@ public class HangMan {
             if (gameWord.charAt(i) == inputChar) {
                 wordAsArray[i] = inputChar;
             }
+        }
+    }
+  public class WordBank {
+
+        Random random = new Random();
+
+        String[] bank = {"homies","pickles","juice","oranges","grapes"};
+
+        public String randomWord() {
+            String randomWord = bank[random.nextInt(5)];
+            return randomWord;
         }
     }
     //GETTERS
